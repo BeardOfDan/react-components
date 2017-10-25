b82ae851beff14334a4dea0a504567cd33b4712c
Daniel Roberts <BeardOfDan@gmail.com>
2017-10-25 11:04:29 -0700

    Finish tutorial

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
 // TODO
+
+var App = () => (
+  <div>Hello World</div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 'bold': false };
+  }
+
+  toggleBold() {
+    this.setState({ bold: !this.state.bold });
+  }
+
+  render() {
+    let style = {
+      fontWeight: this.state.bold ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.toggleBold.bind(this)} onMouseOut={this.toggleBold.bind(this)} >{this.props.food}</li>
+    );
+  }
+}
+
+class GroceryList extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <ul>
+        <GroceryListItem food={this.props.foods[0]} />
+        <GroceryListItem food={this.props.foods[1]} />
+      </ul>
+    );
+  }
+
+  // render() {
+  //   return (
+  //     <ul>
+  //       <GroceryListItem food="Orange Chicken" />
+  //       <GroceryListItem food="Fried Rice" />
+  //     </ul>
+  //   );
+  // }
+
+}
+
+ReactDOM.render(<GroceryList foods={['Orange Chicken', 'Fried Rice']} />, document.getElementById('groceries'));
